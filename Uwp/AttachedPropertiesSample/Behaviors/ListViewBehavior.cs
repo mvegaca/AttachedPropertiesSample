2b401f9d504279de7c4befe60feb2f3a556cc81e
Martin Vega <mvegaca@outlook.com>
2019-10-01 16:36:47 +0200

    Add uwp version

---
+++ Uwp/AttachedPropertiesSample/Behaviors/ListViewBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace AttachedPropertiesSample.Behaviors
+{
+    public class ListViewBehavior : Behavior<ListView>
+    {
+        public static Collection<string> GetListViewItems(Page item)
+        {
+            return (Collection<string>)item.GetValue(ListViewItemsProperty);
+        }
+
+        public static void SetListViewItems(Page item, Collection<string> value)
+        {
+            item.SetValue(ListViewItemsProperty, value);
+        }
+
+        public static readonly DependencyProperty ListViewItemsProperty =
+            DependencyProperty.RegisterAttached("ListViewItems", typeof(Collection<string>), typeof(ListViewBehavior), new PropertyMetadata(new Collection<string>(), OnListViewItemsPropertyChanged));
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+        }
+
+        internal void Initialize(Frame navigationFrame)
+        {
+            navigationFrame.Navigated += OnNavigated;
+        }
+
+        private void OnNavigated(object sender, NavigationEventArgs e)
+        {
+            // Clear all ListView Items
+            AssociatedObject.Items.Clear();
+
+            var page = e.Content as Page;
+
+            // THE ISSUE
+            //
+            // This should get the items provided from the navigated page,
+            // instead of that, it gets items provided for all pages in the navigation history
+            var items = GetListViewItems(page);
+            foreach (var item in items)
+            {
+                AssociatedObject.Items.Add(item);
+            }
+        }
+
+        private static void OnListViewItemsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // This should be called when the property is dynamically updated
+            //
+            // This should be never called on this project
+            // because ListViewItems are statically set from XAML
+        }
+    }
+}
---
+++ Uwp/AttachedPropertiesSample/ShellPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AttachedPropertiesSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ShellPage : Page
+    {
+        public ShellPage()
+        {
+            this.InitializeComponent();
+            listViewBehavior.Initialize(navigationFrame);
+        }
+
+        private void MainPageClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            navigationFrame.Navigate(typeof(MainPage));
+        }
+
+        private void SecondaryPageClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            navigationFrame.Navigate(typeof(SecondaryPage));
+        }
+    }
+}
